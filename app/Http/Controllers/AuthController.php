3646dc19d28b8d339697221e97d55b980eb338de
NgocMy1806 <nguyenngocmy1806@gmail.com>
2023-06-06 16:32:03 +0700

    completeALBCallback

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Models\Customer;
+use Illuminate\Support\Str;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class AuthController extends Controller
 {
@@ -17,15 +20,22 @@ class AuthController extends Controller
         //Mjk0ODlqbmltajZxc244NHFzb2t1NjN1bmg6NnEzdmVnbnUwbGxzcG0wZGgxcDd2YjRjc25rdjA3ZTNwaWgyOGY1OWFqMTI1Z3BwMTk=
 
         session()->put('previousUrl', $currentUrl);
+        if(env('APP_ENV')=='local')
+        {
         $loginUrl = env('COGNITO_LOGIN_URL');
         return redirect($loginUrl);
+        }
+        else
+        {
+        return redirect()->route('handleALBCallback');
+        }
     }
 
     public function handleCognitoCallback(Request $request)
     {
         try {
             $code = $request->query('code');
-            // dd($code);
+            
             //get access token
             $credentials = base64_encode(env('COGNITO_CLIENT_ID') . ':' . env('COGNITO_CLIENT_SECRET'));
             $tokenUrl = env('COGNITO_TOKEN_URL');
@@ -46,8 +56,7 @@ class AuthController extends Controller
             $body = $tokenResponse->body();
             // dump($status, $body);
             $accessToken = $tokenResponse->json()['access_token'];
-            // dump($accessToken);
-
+           
             //get user infor by userinfor endpoint
             $userInforUrl = env('COGNITO_USERINFO_URL');
             $userData = Http::withHeaders([
@@ -100,22 +109,62 @@ class AuthController extends Controller
 
     public function handleALBCallback(Request $request)
 {
-    // Retrieve user information from ALB headers
+     // Retrieve user information from ALB headers
     $accessToken = $request->header('x-amzn-oidc-accesstoken');
     $identity = $request->header('x-amzn-oidc-identity');
-    $userData = json_decode(base64_decode($request->header('x-amzn-oidc-data')), true);
+    $encodedJwt = $request->header('x-amzn-oidc-data');
 
-    // Extract user details from the user claims
-    $userName = $userData['name'];
-    $userEmail = $userData['email'];
+    // Step 1: Get the key ID from JWT headers (the kid field)
+    $jwtHeaders = explode('.', $encodedJwt)[0];
+    $decodedJwtHeaders = base64_decode($jwtHeaders);
+    $decodedJson = json_decode($decodedJwtHeaders, true);
+    $kid = $decodedJson['kid'];
 
-    // Store the user information in the session or perform any other necessary actions
-    session()->put('accessToken', $accessToken);
-    session()->put('userName', $userName);
-    session()->put('userEmail', $userEmail);
+    // Step 2: Get the public key from the regional endpoint
+    $region = 'us-east-1';
+    $url = "https://public-keys.auth.elb.$region.amazonaws.com/$kid";
+    $response = Http::get($url);
+    $pubKey = $response->body();
 
-    // Redirect the user to the desired page
-    return redirect('/dashboard');
+ //step3
+ $algorithms='ES256';
+ $payload = JWT::decode($encodedJwt, new Key($pubKey, $algorithms));
+// dump($payload);
+ $userName = $payload->name;
+ $cognitoId = $payload->sub;
+ $userEmail = $payload->email; 
+
+               //cognito ID
+                $customer = Customer::where('email', $userEmail)->first();
+
+                if (!$customer) {
+                    // Customer does not exist, create a new record
+                    $customer = Customer::create([
+                        'name'=> $userName,
+                        'email' => $userEmail,
+                        'cognito_id' => $cognitoId, 
+                    ]);
+                }
+                elseif(isset($customer) && $customer->cognito_id ==null)
+                {
+                    $customer->update([
+                        'cognito_id' => $cognitoId,
+                    ]);
+                }
+
+                $userId = $customer->id;
+                // dd($userId);
+                session()->put('userName', $userName);
+                session()->put('userId', $userId);
+                // Use the access token for further requests or store it in the session
+                if (session()->has('previousUrl')) {
+                    $previousUrl = session()->pull('previousUrl');
+                    return redirect($previousUrl);
+                } else {
+                    return redirect('/index');
+                }
+
+
 }
 
     public function logout(Request $request)
