9a2eedc6c4ae66b7d924cd13b59bf133cf548b66
NgocMy1806 <nguyenngocmy1806@gmail.com>
2023-06-12 10:17:16 +0000

    remove force scheme filesystem of s3 to http

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -22,20 +22,39 @@ class Media extends Model
         return $this->morphTo();
     }
 
+    // public function getUrlAttribute()
+    // {
+    //     if (env('APP_ENV') !== 'local') {
+    //         if ($this->type == "product_image") {
+    //             return Storage::temporaryUrl("images" . "/" . $this->attributes['name'], '+6000 minutes');
+    //         } else {
+    //             return Storage::temporaryUrl("thumbs" . "/" . $this->attributes['name'], '+6000 minutes');
+    //         }
+    //     } else {
+    //         if ($this->type == "product_image") {
+    //             return asset('storage/images/' . $this->attributes['name']);
+    //         } else {
+    //             return asset('storage/thumbs/' . $this->attributes['name']);
+    //         }
+    //     }
+    // }
+    
     public function getUrlAttribute()
-    {
-        if (env('APP_ENV') !== 'local') {
-            if ($this->type == "product_image") {
-                return Storage::temporaryUrl("images" . "/" . $this->attributes['name'], '+6000 minutes');
-            } else {
-                return Storage::temporaryUrl("thumbs" . "/" . $this->attributes['name'], '+6000 minutes');
-            }
+{
+    if (env('APP_ENV') !== 'local') {
+        $cloudFrontDomain = env('CLOUDFRONT_DOMAIN');
+        if ($this->type == "product_image") {
+            return $cloudFrontDomain . "/images/" . $this->attributes['name'];
         } else {
-            if ($this->type == "product_image") {
-                return asset('storage/images/' . $this->attributes['name']);
-            } else {
-                return asset('storage/thumbs/' . $this->attributes['name']);
-            }
+            return $cloudFrontDomain . "/thumbs/" . $this->attributes['name'];
+        }
+    } else {
+        if ($this->type == "product_image") {
+            return asset('storage/images/' . $this->attributes['name']);
+        } else {
+            return asset('storage/thumbs/' . $this->attributes['name']);
         }
     }
+}
+
 }
