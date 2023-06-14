b9312cb3ed907b86d83909adb24cabb7c37d0cea
NgocMy1806 <nguyenngocmy1806@gmail.com>
2023-06-14 03:36:41 +0000

    apply CF

--- resources/views/user/layout/layout.blade.php
+++ resources/views/user/layout/layout.blade.php
@@ -55,7 +55,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 </head>
 
 <body>
-    <div class="banner" style="min-height:341.510px; margin-bottom: 40px;">
+    <!--<div class="banner" style="min-height:341.510px; margin-bottom: 40px;">-->
+    <!--<div class=" {{ env('APP_ENV') !== 'local' ? 'banner-cloudfront' : 'banner-local' }} banner" style="min-height:341.510px; margin-bottom: 40px;">-->
+        <div class="banner" style="{{ env('APP_ENV') !== 'local' ? 'background: url('.env('CLOUDFRONT_DOMAIN').'/common/3.jpg) no-repeat center top' : 'background: url(../images/3.jpg) no-repeat center top' }}; min-height: 341.510px; margin-bottom: 40px;">
+
         <div class="container">
             @include('user.layout.components.headertop')
             <div class="header_bottom">
